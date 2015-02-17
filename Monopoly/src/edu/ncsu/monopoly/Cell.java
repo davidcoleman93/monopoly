f0f7a74b1ef3900bd1a3c279317fbe48d1057014
davidcoleman93 <c11456862@mydit.ie>
2015-02-17 16:49:07 +0000

    Commit #2. Donen tasks 2 and 3.

--- Monopoly/src/edu/ncsu/monopoly/Cell.java
+++ Monopoly/src/edu/ncsu/monopoly/Cell.java
@@ -1,41 +1,41 @@
 package edu.ncsu.monopoly;
 
 public abstract class Cell {
-	private boolean available = true;
+	protected boolean available = true;
 	private String name;
-	protected Player owner;
+	protected Player theOwner;
 
 	public String getName() {
 		return name;
 	}
 
-	public Player getOwner() {
-		return owner;
+	public Player getTheOwner() {
+		return theOwner;
 	}
 	
 	public int getPrice() {
 		return 0;
 	}
 
-	public boolean isAvailable() {
-		return available;
-	}
-	
 	public abstract void playAction();
 
-	public void setAvailable(boolean available) {
-		this.available = available;
-	}
-	
 	void setName(String name) {
 		this.name = name;
 	}
 
-	public void setOwner(Player owner) {
-		this.owner = owner;
+	public void setTheOwner(Player owner) {
+		this.theOwner = owner;
 	}
     
     public String toString() {
         return name;
     }
+
+	public boolean isAvailable() {
+		return available;
+	}
+
+	public void setAvailable(boolean available) {
+		this.available = available;
+	}
 }
